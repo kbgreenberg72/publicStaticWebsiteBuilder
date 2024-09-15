2b3db8a8b9f7ee43968ee48feab800632a6fa45f
kbgreenberg72 <kyle.greenberg93@gmail.com>
2024-09-15 15:36:35 -0400

    start of another project

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+def main():
+    test = TextNode("This is a test node", "bold", "https://www.boot.dev")
+    
+    print(test)
+
+if __name__ == "__main__":
+    main()
